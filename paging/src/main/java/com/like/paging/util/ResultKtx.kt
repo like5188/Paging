9b009f030666cf318b5e2d2c8bb07aad7ac2389d
like5188 <260864866@qq.com>
2021-08-02 10:08:56 +0800

    增加辅助工具类ResultKtx.kt

--- paging/src/main/java/com/like/paging/util/ResultKtx.kt
+++ paging/src/main/java/com/like/paging/util/ResultKtx.kt
@@ -3,88 +3,38 @@ package com.like.paging.util
 import com.like.paging.RequestState
 import com.like.paging.RequestType
 import com.like.paging.Result
-import com.like.paging.ResultReport
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
 
 /**
- * [com.like.paging.Result] 扩展功能，方便绑定到 RecyclerView。
- */
-
-/**
- * 开始搜集数据。
- *
- * @param onFailed      失败回调，如果需要做其它错误处理，可以从这里获取。
- * @param onSuccess     成功回调，如果需要结果做其它逻辑处理，可以从这里获取。
- */
-suspend fun <ResultType> Result<ResultType>.collect(
-    onFailed: (suspend (RequestType, Throwable) -> Unit)? = null,
-    onSuccess: (suspend (RequestType, ResultType) -> Unit)? = null,
-) {
-    resultReportFlow.collect { resultReport ->
-        val state = resultReport.state
-        val type = resultReport.type
-        when (state) {
-            is RequestState.Failed -> {
-                onFailed?.invoke(type, state.throwable)
-            }
-            is RequestState.Success<ResultType> -> {
-                onSuccess?.invoke(type, state.data)
-            }
-        }
-    }
-}
-
-/**
- * 绑定进度条。
- * 初始化或者刷新时控制进度条的显示隐藏。
- *
- * @param show          初始化或者刷新开始时显示进度条
- * @param hide          初始化或者刷新成功或者失败时隐藏进度条
- */
-fun <ResultType> Result<ResultType>.bindProgress(
-    show: () -> Unit,
-    hide: () -> Unit,
-): Result<ResultType> {
-    val newResultReportFlow = resultReportFlow.onEach { resultReport ->
-        val state = resultReport.state
-        val type = resultReport.type
-        if (type is RequestType.Initial || type is RequestType.Refresh) {
-            when (state) {
-                is RequestState.Running -> {
-                    show()
-                }
-                else -> {
-                    hide()
-                }
-            }
-        }
-    }
-    return updateResultReportFlow(newResultReportFlow)
-}
-
-/**
- * 绑定列表数据
+ * 绑定列表数据到界面上
  *
  * @param onData            初始化或者刷新成功并且有数据时回调。
- * @param onEmpty           初始化或者刷新成功并且没有数据时回调。
- * @param onError           初始化失败时回调。
  * @param onLoadMore        加载更多成功并且有数据时回调。
  * @param onLoadMoreEnd     加载更多成功并且没有数据时回调。
  * @param onLoadMoreError   加载更多失败时回调。
+ * @param onEmpty           初始化或者刷新成功并且没有数据时回调。
+ * @param onError           初始化失败时回调。
+ * @param show              初始化或者刷新开始时显示进度条
+ * @param hide              初始化或者刷新成功或者失败时隐藏进度条
+ * @param onFailed          失败回调，如果需要做其它错误处理，可以从这里获取。
+ * @param onSuccess         成功回调，如果需要结果做其它逻辑处理，可以从这里获取。
  */
-fun <ValueInList> Result<List<ValueInList>?>.bindList(
+fun <ValueInList> Result<List<ValueInList>?>.bind(
     onData: (List<ValueInList>) -> Unit,
+    onLoadMore: (List<ValueInList>) -> Unit,
+    onLoadMoreEnd: () -> Unit,
+    onLoadMoreError: (Throwable) -> Unit,
     onEmpty: (() -> Unit)? = null,
     onError: ((Throwable) -> Unit)? = null,
-    onLoadMore: ((List<ValueInList>) -> Unit)? = null,
-    onLoadMoreEnd: (() -> Unit)? = null,
-    onLoadMoreError: ((Throwable) -> Unit)? = null,
+    show: (() -> Unit)? = null,
+    hide: (() -> Unit)? = null,
+    onFailed: (suspend (RequestType, Throwable) -> Unit)? = null,
+    onSuccess: (suspend (RequestType, List<ValueInList>?) -> Unit)? = null,
 ): Result<List<ValueInList>?> {
-    val newResultReportFlow = resultReportFlow.onEach { resultReport ->
+    resultReportFlow.onEach { resultReport ->
         val state = resultReport.state
         val type = resultReport.type
+        // 数据处理
         when {
             (type is RequestType.Initial || type is RequestType.Refresh) && state is RequestState.Success -> {
                 val list = state.data
@@ -114,10 +64,26 @@ fun <ValueInList> Result<List<ValueInList>?>.bindList(
                 }
             }
         }
+        // 进度处理
+        if (type is RequestType.Initial || type is RequestType.Refresh) {
+            when (state) {
+                is RequestState.Running -> {
+                    show?.invoke()
+                }
+                else -> {
+                    hide?.invoke()
+                }
+            }
+        }
+        // 成功失败回调处理
+        when (state) {
+            is RequestState.Failed -> {
+                onFailed?.invoke(type, state.throwable)
+            }
+            is RequestState.Success -> {
+                onSuccess?.invoke(type, state.data)
+            }
+        }
     }
-    return updateResultReportFlow(newResultReportFlow)
-}
-
-fun <ResultType> Result<ResultType>.updateResultReportFlow(newResultReportFlow: Flow<ResultReport<ResultType>>): Result<ResultType> {
-    return Result(newResultReportFlow, initial, refresh, retry, loadAfter, loadBefore)
+    return this
 }
