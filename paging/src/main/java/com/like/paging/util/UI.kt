f3734ab318ce4fb7c2b1a64098af5a715182216e
like5188 <260864866@qq.com>
2021-08-04 12:17:58 +0800

    完善

--- paging/src/main/java/com/like/paging/util/UI.kt
+++ paging/src/main/java/com/like/paging/util/UI.kt
@@ -2,7 +2,6 @@ package com.like.paging.util
 
 import com.like.paging.RequestState
 import com.like.paging.RequestType
-import com.like.paging.Result
 import com.like.paging.ResultReport
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
@@ -10,36 +9,26 @@ import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.withContext
 
 /**
- * 绑定数据到界面（通常为 RecyclerView）的扩展工具类。
- */
-
-/**
  * 绑定不分页数据到界面。
- * 执行获取数据的 suspend 函数，并控制进度条的显示隐藏。
+ * 成功失败回调，进度条显示隐藏。
  *
- * @param onData            有数据时回调。
- * @param onEmpty           没有数据时回调。
- * @param onError           失败时回调。
- * @param show              初始化或者刷新开始时显示进度条
- * @param hide              初始化或者刷新成功或者失败时隐藏进度条
+ * @param onData            请求成功后回调。
+ * @param onError           请求失败时回调。
+ * @param show              显示进度条
+ * @param hide              隐藏进度条
  */
-suspend fun <ValueInList> (suspend () -> List<ValueInList>?).bind(
-    onData: (List<ValueInList>?) -> Unit,
-    onEmpty: (() -> Unit)? = null,
-    onError: ((Throwable) -> Unit)? = null,
+suspend fun <ResultType> (suspend () -> ResultType).bind(
+    onData: suspend (ResultType) -> Unit,
+    onError: (suspend (Throwable) -> Unit)? = null,
     show: (() -> Unit)? = null,
     hide: (() -> Unit)? = null,
 ) = withContext(Dispatchers.Main) {
     show?.invoke()
     try {
-        val list = withContext(Dispatchers.IO) {
+        val result = withContext(Dispatchers.IO) {
             this@bind()
         }
-        if (list.isNullOrEmpty()) {
-            onEmpty?.invoke()
-        } else {
-            onData(list)
-        }
+        onData(result)
     } catch (e: Exception) {
         onError?.invoke(e)
     } finally {
@@ -48,43 +37,35 @@ suspend fun <ValueInList> (suspend () -> List<ValueInList>?).bind(
 }
 
 /**
- * 绑定分页数据的 Result 绑定到界面。
+ * 绑定分页数据到界面。
+ * 成功失败回调，进度条显示隐藏。
  *
- * @param onInitialOrRefresh        初始化或者刷新成功并且有数据时回调。
- * @param onLoadMore                加载更多成功并且有数据时回调。
- * @param onLoadMoreEnd             加载更多成功并且没有数据时回调。
- * @param onLoadMoreError           加载更多失败时回调。
- * @param onInitialOrRefreshEmpty   初始化或者刷新成功并且没有数据时回调。
+ * @param onInitialOrRefresh        初始化或者刷新成功时回调。
  * @param onInitialError            初始化失败时回调。
+ * @param onLoadMore                加载更多成功时回调。
+ * @param onLoadMoreError           加载更多失败时回调。
  * @param show                      初始化或者刷新开始时显示进度条
  * @param hide                      初始化或者刷新成功或者失败时隐藏进度条
  * @param onFailed                  失败回调，如果需要做其它错误处理，可以从这里获取。
  * @param onSuccess                 成功回调，如果需要结果做其它逻辑处理，可以从这里获取。
  */
-fun <ValueInList> Result<List<ValueInList>?>.bind(
-    onInitialOrRefresh: (List<ValueInList>) -> Unit,
-    onLoadMore: (List<ValueInList>) -> Unit,
-    onLoadMoreEnd: () -> Unit,
-    onLoadMoreError: (Throwable) -> Unit,
-    onInitialOrRefreshEmpty: (() -> Unit)? = null,
-    onInitialError: ((Throwable) -> Unit)? = null,
+fun <ResultType> Flow<ResultReport<ResultType>>.bind(
+    onInitialOrRefresh: suspend (ResultType) -> Unit,
+    onInitialError: (suspend (Throwable) -> Unit)? = null,
+    onLoadMore: suspend (ResultType) -> Unit,
+    onLoadMoreError: suspend (Throwable) -> Unit,
     show: (() -> Unit)? = null,
     hide: (() -> Unit)? = null,
     onFailed: (suspend (RequestType, Throwable) -> Unit)? = null,
-    onSuccess: (suspend (RequestType, List<ValueInList>?) -> Unit)? = null,
-): Flow<ResultReport<List<ValueInList>?>> {
-    return resultReportFlow.onEach { resultReport ->
+    onSuccess: (suspend (RequestType, ResultType) -> Unit)? = null,
+): Flow<ResultReport<ResultType>> {
+    return onEach { resultReport ->
         val state = resultReport.state
         val type = resultReport.type
         // 数据处理
         when {
             (type is RequestType.Initial || type is RequestType.Refresh) && state is RequestState.Success -> {
-                val list = state.data
-                if (list.isNullOrEmpty()) {
-                    onInitialOrRefreshEmpty?.invoke()
-                } else {
-                    onInitialOrRefresh(list)
-                }
+                onInitialOrRefresh(state.data)
             }
             type is RequestType.Initial && state is RequestState.Failed -> {
                 onInitialError?.invoke(state.throwable)
@@ -92,13 +73,7 @@ fun <ValueInList> Result<List<ValueInList>?>.bind(
             type is RequestType.After || type is RequestType.Before -> {
                 when (state) {
                     is RequestState.Success -> {
-                        val list = state.data
-                        if (list.isNullOrEmpty()) {
-                            // 到底了
-                            onLoadMoreEnd()
-                        } else {
-                            onLoadMore(list)
-                        }
+                        onLoadMore(state.data)
                     }
                     is RequestState.Failed -> {
                         onLoadMoreError(state.throwable)
