bd84f657a4c6302d10614ab635c5f1aab2e46c50
like5188 <260864866@qq.com>
2021-11-16 17:28:44 +0800

    重构

--- paging/src/main/java/com/like/paging/Result.kt
+++ paging/src/main/java/com/like/paging/Result.kt
@@ -7,11 +7,11 @@ import kotlinx.coroutines.flow.Flow
  */
 data class Result<ResultType>(
     // 初始化操作
-    var initial: () -> Flow<ResultType>,
+    val initial: () -> Flow<ResultType>,
     // 刷新操作
-    var refresh: () -> Flow<ResultType>,
+    val refresh: () -> Flow<ResultType>,
     // 往后加载更多，不分页时不用设置
-    var loadAfter: (() -> Flow<ResultType>)? = null,
+    val loadAfter: (() -> Flow<ResultType>)? = null,
     // 往前加载更多，不分页时不用设置
-    var loadBefore: (() -> Flow<ResultType>)? = null
+    val loadBefore: (() -> Flow<ResultType>)? = null
 )
