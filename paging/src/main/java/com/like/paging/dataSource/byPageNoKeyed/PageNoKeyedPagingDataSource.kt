5594f4b111f5168692acb2299278e1cf5e9a8e2f
like5188 <260864866@qq.com>
2021-11-17 11:15:21 +0800

    重构

--- paging/src/main/java/com/like/paging/dataSource/byPageNoKeyed/PageNoKeyedPagingDataSource.kt
+++ paging/src/main/java/com/like/paging/dataSource/byPageNoKeyed/PageNoKeyedPagingDataSource.kt
@@ -6,32 +6,32 @@ import com.like.paging.dataSource.PagingDataSource
 /**
  * 根据自己维护的 pageNo 来作为分页 key 的分页数据源。
  *
- * @param firstPage     初始页码。默认为 1
- * @param lastPage      最后页码。默认为 [Int.MAX_VALUE]
+ * @param initPage      往前或者往后的初始页码。默认为 1
+ * @param limitPage     往前或者往后的极限页码。默认为 [Int.MAX_VALUE]
  * @param pageSize      每页加载数量。默认为 10
  */
 abstract class PageNoKeyedPagingDataSource<ResultType>(
-    private val firstPage: Int = 1,
-    private val lastPage: Int = Int.MAX_VALUE,
+    private val initPage: Int = 1,
+    private val limitPage: Int = Int.MAX_VALUE,
     private val pageSize: Int = 10
 ) : PagingDataSource<ResultType>() {
-    private var pageNo: Int = firstPage
+    private var pageNo: Int = initPage
 
     final override suspend fun load(requestType: RequestType): ResultType {
         when (requestType) {
             is RequestType.Initial, is RequestType.Refresh -> {
-                pageNo = firstPage
+                pageNo = initPage
             }
             is RequestType.Before -> {
                 pageNo--
-                if (pageNo < firstPage) {
-                    pageNo = firstPage
+                if (pageNo < limitPage) {
+                    pageNo = limitPage
                 }
             }
             is RequestType.After -> {
                 pageNo++
-                if (pageNo > lastPage) {
-                    pageNo = lastPage
+                if (pageNo > limitPage) {
+                    pageNo = limitPage
                 }
             }
         }
@@ -42,14 +42,14 @@ abstract class PageNoKeyedPagingDataSource<ResultType>(
             when (requestType) {
                 is RequestType.Before -> {
                     pageNo++
-                    if (pageNo > lastPage) {
-                        pageNo = lastPage
+                    if (pageNo > initPage) {
+                        pageNo = initPage
                     }
                 }
                 is RequestType.After -> {
                     pageNo--
-                    if (pageNo < firstPage) {
-                        pageNo = firstPage
+                    if (pageNo < initPage) {
+                        pageNo = initPage
                     }
                 }
             }
