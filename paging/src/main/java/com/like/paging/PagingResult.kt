572ecdd14013bdd8b6290789ddb0530256ca3c0b
like5188 <260864866@qq.com>
2021-11-18 22:34:35 +0800

    重构

--- paging/src/main/java/com/like/paging/PagingResult.kt
+++ paging/src/main/java/com/like/paging/PagingResult.kt
@@ -1,7 +1,12 @@
 package com.like.paging
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
 
+/**
+ * @param flow              数据流。在每次调用[Flow.collect]来触发请求之前都需要调用[setRequestType]设置请求类型
+ * @param setRequestType    设置请求类型
+ */
 data class PagingResult<ResultType>(
     var flow: Flow<ResultType>,
     val setRequestType: (RequestType) -> Unit,
