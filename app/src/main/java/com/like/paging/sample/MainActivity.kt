82128b5e144432cd8de48f882665f990eedcc7a8
like5188 <260864866@qq.com>
2021-11-19 10:46:03 +0800

    重构

--- app/src/main/java/com/like/paging/sample/MainActivity.kt
+++ app/src/main/java/com/like/paging/sample/MainActivity.kt
@@ -4,35 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import com.like.common.util.Logger
 import com.like.paging.sample.paging.view.PagingActivity
-import com.like.paging.util.ConcurrencyHelper
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.yield
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val concurrencyHelper = ConcurrencyHelper()
-
-        (0..99).forEach {
-            lifecycleScope.launch {
-                concurrencyHelper.dropIfPreviousRunning {
-                    delay((it - 100L) * -1)
-                    test(it)
-                }
-            }
-        }
-    }
-
-    private suspend fun test(i: Int): Int {
-        Logger.d("执行 $i")
-        yield()
-        return i
     }
 
     fun startPagingActivity(view: View) {
